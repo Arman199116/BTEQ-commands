3ef349f34f21ef0a703bd18a2de4e25c31ebd8f9
Arman199116 <47755796+Arman199116@users.noreply.github.com>
2019-10-04 12:06:26 +0400

    Change psql tabs to 4 spaces (#6)

--- postgresql-11.5/src/bin/psql/commandbteq.h
+++ postgresql-11.5/src/bin/psql/commandbteq.h
@@ -7,7 +7,7 @@
  */
 #ifndef COMMAND_BTEQ_H
 #define COMMAND_BTEQ_H
-
+    
 #include "fe_utils/printbteq.h"
 #include "fe_utils/psqlscan.h"
 #include "fe_utils/conditional.h"
@@ -15,23 +15,23 @@
 
 typedef enum _dotResult
 {
-	BTEQ_CMD_UNKNOWN = 0,		/* not done parsing yet (internal only) */
-	BTEQ_CMD_SEND,				/* query complete; send off */
-	BTEQ_CMD_SKIP_LINE,			/* keep building query */
-	BTEQ_CMD_TERMINATE,			/* quit program */
-	BTEQ_CMD_NEWEDIT,			/* query buffer was changed (e.g., via \e) */
-	BTEQ_CMD_ERROR				/* the execution of the dot command
-								 * resulted in an error */
+    BTEQ_CMD_UNKNOWN = 0,        /* not done parsing yet (internal only) */
+    BTEQ_CMD_SEND,                /* query complete; send off */
+    BTEQ_CMD_SKIP_LINE,            /* keep building query */
+    BTEQ_CMD_TERMINATE,            /* quit program */
+    BTEQ_CMD_NEWEDIT,            /* query buffer was changed (e.g., via \e) */
+    BTEQ_CMD_ERROR                /* the execution of the dot command
+                                 * resulted in an error */
 } dotResult;
 
 
 extern dotResult HandleDotCmds(PsqlScanState scan_state,
-				ConditionalStack cstack,
-				PQExpBuffer query_buf,
-				PQExpBuffer previous_buf);
+                ConditionalStack cstack,
+                PQExpBuffer query_buf,
+                PQExpBuffer previous_buf);
 
 extern void connection_warningsbteq(bool in_startup);
 extern void SyncVariablesbteq(void);
 
 
-#endif							/* COMMAND_BTEQ_H */
+#endif                            /* COMMAND_BTEQ_H */
